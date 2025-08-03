9efc72199e788851a9b4d6c9d2ed021785fae937
summerpun2803 <mastersam2003@gmail.com>
2025-08-03 13:05:30 +0530

    PMM

--- src/kernel/arch/memManager/mem.c
+++ src/kernel/arch/memManager/mem.c
@@ -3,9 +3,10 @@
 
 #define PAGE_SIZE 4096
 #define BITMAP_ENTRY_BITS 32
+#define OFFSET 0x100000
 
 static uint32_t* bitmap = 0;
-static uint64_t total_pages = 0;
+static uint64_t total_blocks = 0;
 static uint64_t bitmap_size_in_bytes = 0;
 static uint64_t memory_base = 0; 
 
@@ -21,17 +22,55 @@ static inline void bitmap_clear(uint32_t bit) {
 static inline int bitmap_test(uint32_t bit) {
     return bitmap[bit / 32] & (1 << (bit % 32));
 }
-static inline void bitmap_allocate(uint64_t start, uint64_t end) {
+static inline void bitmap_allocate_addr(uint64_t start, uint64_t end) {
 
     for(uint64_t i=start; i<=end; i++){
         bitmap_set(i);
     }
 }
-static inline void bitmap_deallocate(uint64_t start, uint64_t end) {
+static inline void bitmap_deallocate_addr(uint64_t start, uint64_t end) {
 
     for(uint64_t i=start; i<=end; i++){
         bitmap_clear(i);
     }
+}
+
+void* alloc_block(){
+
+    int temp = first_free();
+    if(temp == -1) return 0;
+
+    bitmap_set(temp);
+
+    uint32_t * addr = temp * PAGE_SIZE;
+    // printf("allocating : %d\n", temp);
+    return (void *)addr;
+}
+
+void dealloc_block(void* addr){
+
+    int temp = (uint32_t)addr / PAGE_SIZE;    
+    // printf("deallocating : %d\n", temp);
+    bitmap_clear(temp);
+}
+
+int first_free(){
+
+    for(uint32_t i=0; i<bitmap_size_in_bytes/32; i++){
+
+        if(bitmap[i] != 0xffffffff){
+
+            for(int j=0; j<32; j++){
+                
+                int temp = 1 << j;
+                if(! (bitmap[i] & temp)){
+                    return i*32 + j;
+                }
+            }
+        }
+    }
+
+    return -1;
 }
 
 
@@ -47,25 +86,19 @@ void BitMap_Init(MemoryInfo* meminfo){
         }
     }
 
-    memory_base = U_base;
-    total_pages =( U_length - 0x100000)/ PAGE_SIZE;
-    bitmap_size_in_bytes = (total_pages + 7) / 8;
+    memory_base = U_base + OFFSET;
+    total_blocks =( U_length - OFFSET)/ PAGE_SIZE;
+    bitmap_size_in_bytes = (total_blocks + 7) / 8;
 
-    bitmap = (uint32_t *)( memory_base  + 0x100000);
-    bitmap_deallocate(0, bitmap_size_in_bytes);
-    // printf("DEBUG: Small memset completed\n");
+    bitmap = (uint32_t *)( memory_base );
+    bitmap_deallocate_addr(0, bitmap_size_in_bytes);
+
     uint32_t bitmap_metadata = (bitmap_size_in_bytes + PAGE_SIZE - 1) / PAGE_SIZE;
-    bitmap_allocate(0, bitmap_metadata);
-
-    if(bitmap_test(bitmap_metadata + 1)) {
-        printf("Bit is SET (page is allocated)\n");
-    } else {
-        printf("Bit is CLEAR (page is free)\n");
-    }
+    bitmap_allocate_addr(0, bitmap_metadata);
 
     printf("Bitmap Allocator Init:\n");
     printf(" Usable memory base: 0x%l\n", U_base);
-    printf(" Total pages: %d\n", total_pages);
+    printf(" Total pages: %d\n", total_blocks);
     printf(" Bitmap size: %d bytes\n", bitmap_size_in_bytes);
     printf(" Bitmap pages used: %d KB\n", bitmap_metadata);
 
